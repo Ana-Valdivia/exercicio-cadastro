a5b28eef311b58745dc5a3e39ed52c3c1820cfac
ana-valdivia <ana.raquel1291@hotmail.com>
2019-09-09 22:33:56 -0300

    Exercicio Cadastro

---
+++ app/src/main/java/com/example/cadastro/DadosComplementares.java
@@ -0,0 +1,13 @@
+package com.example.cadastro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DadosComplementares extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dados_complementares);
+    }
+}
